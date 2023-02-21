c3af49c4d7b001b5c0ff793ff48cdea74efca690
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-21 16:46:29 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryParent = document.querySelector('.gallery');
 
-console.log(galleryItems);
+// * add images
+const images = galleryItems.map(item => `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join("");
+galleryParent.innerHTML = images;
+
+const loadImageFullSize = (event) => {
+    event.preventDefault();
+    const selectedImage = event.target.dataset.source;
+    const largePhoto = basicLightbox.create(
+        `<img src="${selectedImage}" width="1280">`, );
+    largePhoto.show();
+}
+
+galleryParent.addEventListener("click", loadImageFullSize);
