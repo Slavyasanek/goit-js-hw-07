5557649456f99e1b4c4dde768716f55efc6997a3
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-21 21:38:13 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryParent = document.querySelector('.gallery');
+// * add images
+const imagesLightBox = galleryItems.map(item => `
+<a class="gallery__item" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+
+galleryParent.innerHTML = imagesLightBox;
+// * add gallery 
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
